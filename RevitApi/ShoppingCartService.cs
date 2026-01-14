9a047f400c17be49822ce506b3bcea5432e5c934
matveeva-aa <adel.matveeva@gmail.com>
2026-01-14 19:16:10 +0300

    Task_01

---
+++ RevitApi/ShoppingCartService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_01_1
+{
+    public class ShoppingCartService
+    {
+        public decimal CalculateTotalPrice(string customerType, List<decimal> itemPrices)
+        {
+            decimal baseTotal = 0;
+
+            // Нарушение DRY: дублирование кода
+            for (int i = 0; i < itemPrices.Count; i++)
+            {
+                baseTotal += itemPrices[i];
+            }
+
+            decimal discount = 0;
+
+            // Нарушение YAGNI: нужна только поддержка типа "Regular". Поддержка "Premium" и "VIP" лишние
+
+            if (customerType == "Regular")
+            {
+                discount = baseTotal * 0.05m; // 5%
+            }
+            else if (customerType == "Premium")
+            {
+                discount = baseTotal * 0.15m; // 15%
+
+                // Похоже нарушение KISS: внутри логики "Premium" доп. логика
+
+                if (discount > 1000)
+                {
+                    discount = 1000 + (discount - 1000) * 0.1m;
+                }
+            }
+            else if (customerType == "VIP")
+            {
+                discount = baseTotal * 0.20m; // 20%
+            }
+
+            decimal finalPrice = baseTotal - discount;
+
+            Console.WriteLine($"Base: {baseTotal}, Discount: {discount}, Final: {finalPrice}");
+            return finalPrice;
+        }
+
+        public decimal CalculateTotalPriceWithQuantities(string customerType, Dictionary<decimal, int> itemsWithQuantities)
+        {
+            List<decimal> allPrices = new List<decimal>();
+
+            // Нарушение KISS: нужно умножить "цена * количество",
+
+            foreach (var item in itemsWithQuantities)
+            {
+                // нарушение DRY: дублирование кода
+                for (int i = 0; i < item.Value; i++)
+                {
+                    allPrices.Add(item.Key);
+                }
+            }
+            return CalculateTotalPrice(customerType, allPrices);
+        }
+    }
+}
