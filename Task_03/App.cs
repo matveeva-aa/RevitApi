fe164d98ef084a52d0fa78fb08a9b4907a0e3718
matveeva-aa <adel.matveeva@gmail.com>
2026-01-15 22:32:05 +0300

    Task_03

---
+++ Task_03/App.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.UI;
+
+namespace Task_03
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "ПИК";
+            string panelName = "АР";
+
+            try
+            {
+                application.CreateRibbonTab(tabName);
+            }
+            catch
+            {
+            }
+
+            RibbonPanel panel = application.CreateRibbonPanel(tabName, panelName);
+
+            var buttonData = new PushButtonData(
+                "OpeningsCommandButton",
+                "Отверстия",
+                typeof(App).Assembly.Location,
+                "Task_03.OpeningsCommand"
+            );
+
+            panel.AddItem(buttonData);
+
+            return Result.Succeeded;
+
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
